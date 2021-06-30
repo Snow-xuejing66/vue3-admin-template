697e5fd825e536531e507ee0ee817a25d2ca5af0
张稳 <18655952858@163.com>
2021-06-30 10:40:40 +0800

    去掉不必要的代码

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,21 +9,7 @@ const frameIn = [
 		path: '/',
 		redirect: {
 			name: 'dashboard'
-		},
-		children: [
-			// 页面重定向 必须保留
-			{
-				path: 'redirect/:route*',
-				name: 'redirect',
-				hidden: true,
-				component: {
-					beforeRouteEnter(to, from, next) {
-						next(instance => instance.$router.replace(JSON.parse(from.params.route)))
-					},
-					render: h => h()
-				}
-			}
-		]
+		}
 	},
 	dashboard
 ]
